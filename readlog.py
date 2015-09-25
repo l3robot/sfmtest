65b87f2f5dbe66a56608ae1b2c1961e5a4157ed7
l3robot <emile.courriel@gmail.com>
2015-09-25 14:26:57 -0400

    add option to read all log file at once

--- readlog.py
+++ readlog.py
@@ -3,6 +3,20 @@
 import src.sfmtest
 
 import sys
+from os.path import join, isdir
+from os import listdir
+
+def all_logs(dirpath):
+
+	dirs = [join(dirpath, im) for im in listdir(dirpath) if isdir(join(dirpath, im))]
+
+	for di in dirs:
+		head = "FOR {0}".format(di)
+		thelen = len(head) 
+		print(head)
+		print("-"*thelen)
+		sfmtest.read_log(di)
+		print()
 
 if __name__ == '__main__':
 
@@ -10,6 +24,19 @@ if __name__ == '__main__':
 		print("You must give a directory name")
 		exit(-1)
 
+	if len(sys.argv) > 3:
+		option = sys.argv[2]
+	else:
+		option = "--one"
+
 	dirpath = sys.argv[1]	
 
-	sfmtest.read_log(dirpath)
+	if option == "--all":
+		all_logs(dirpath)
+	else:
+		head = "FOR {0}".format(dirpath)
+		thelen = len(head) 
+		print(head)
+		print("-"*thelen)
+		sfmtest.read_log(dirpath)
+		print()
